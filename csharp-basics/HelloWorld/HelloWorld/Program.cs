7cd4b2a494a8149aca19511dde23a9c57daf43b0
yangjie6020 <yangjie6020@gmail.com>
2020-09-13 21:56:08 +0800

    first commit

---
+++ csharp-basics/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey();
+
+            // C# 语言特性
+            // Bool Conditions
+            // Automatic Garbage Collection
+            // Standard Library
+            // Assembly Versioning
+            // Properties and Events
+            // Delegates and Event Management
+            // Generics
+            // Indexers
+            // Conditional Compilation
+            // Multithreading
+            // LINQ and Lambda
+            // Windows Library
+
+            //.NET框架适用的范围:
+            // Windows应用程序
+            // Web应用程序
+            // Web服务
+
+            // .NET框架的适用的语言: C#, C++, Visual Basic, JScript, COBOL
+            // .NET框架的组件
+            // 公共语言运行库(Common Language Runtime - CLR)
+            // .NET框架类(.NET Framework Class Library)
+            // 公共语言规范(Common Language Specification)
+            // 通用类型系统(Common Type System)
+            // 元数据和组件 (Metadata & Assemblies)
+            // Windows窗体 (Windows Forms)
+            // ASP.NET 和 ASP.NET AJAX
+            // ADO.NET
+            // Windows工作流基础 (Windows Workflow Foundation - WF)
+            // Windows显示基础 (Windows Presentation Foundation - WPF)
+            // Windows通信基础 (Windows Communication Foundation - WCF)
+            // LINQ
+        }
+    }
+}
---
+++ csharp-basics/SyntaxApp/SyntaxApp/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+// using 使用System空间
+
+namespace SyntaxApp
+{
+    class Rectangle
+    {
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 5.5;
+            width = 4.5;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syntax Test!!!");
+
+            Rectangle r = new Rectangle();
+            r.Acceptdetails();
+            r.Display();
+
+
+            // C#数据类型, Data Type
+            // 值类型 Values Types
+            // 引用类型 Reference Types
+            // 指针类型 Pointer Types
+
+            // Values Types
+            // bool : true or false
+            // byte : 0 - 255
+            // char :
+            // decimal
+            // double
+            // float
+            // int
+            // long
+            // sbyte
+            // short
+            // unit
+            // ulong 
+            // ushort
+            Console.WriteLine("\n------------ Values Types -------------------\n");
+            Console.WriteLine("sizeof(bool) {0}", sizeof(bool));
+            Console.WriteLine("sizeof(byte) {0}", sizeof(byte));
+            Console.WriteLine("sizeof(char) {0}", sizeof(char));
+            Console.WriteLine("sizeof(devimal) {0}", sizeof(decimal));
+            Console.WriteLine("sizeof(double) {0}", sizeof(double));
+            Console.WriteLine("sizeof(float) {0}", sizeof(float));
+            Console.WriteLine("sizeof(int) {0}", sizeof(int));
+            Console.WriteLine("sizeof(long) {0}", sizeof(long));
+            Console.WriteLine("sizeof(sbyte) {0}", sizeof(sbyte));
+            Console.WriteLine("sizeof(short) {0}", sizeof(short));
+            Console.WriteLine("sizeof(uint) {0}", sizeof(uint));
+            Console.WriteLine("sizeof(ulong) {0}", sizeof(ulong));
+            Console.WriteLine("sizeof(ushort) {0}", sizeof(ushort));
+
+            Console.WriteLine("\n ----------------------- Type Change -----------------------------------\n");
+            double d = 5789.11;
+            int d1 = (int)d;
+            Console.WriteLine("d -> d1 => {0}", d1);
+            //
+            // Type Change Method
+            // ToBoolean
+            // ToByte
+            // ToChar
+            // ToDateTime
+            // ToDecimal
+            // ToDouble
+            // ToInt16
+            // ToInt32
+            // ToInt64
+            // ToSbyte
+            // ToSingle
+            // ToString
+            // ToType
+            // ToUInt16
+            // ToUInt32
+            // ToUInt64
+
+            Console.WriteLine("d to String {0}", d.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
